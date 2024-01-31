8cedeb9cfc6cdae825d2401b8d7f57ffad50d586
zdravkokirilov <zdravkovasilchevkirilov@gmail.com>
2024-01-31 09:35:10 +0200

    start adding entity models

---
+++ src/models.ts
@@ -0,0 +1,81 @@
+enum ProjectId {
+  /* ISG */
+  DynamicFormBuilder = "dynamic-form-builder",
+
+  /* KPMG */
+  D3Analytics = "d3-analytics",
+  CompanyOrgChart = "company-org-chart",
+  CompanyPhoneManagement = "company-phone-management",
+
+  /* Droxic */
+  CMSApp = "cms-app",
+  FintechApp = "fintech-app",
+  IoTApp = "iot-app",
+  SportsApp = "sports-app",
+
+  /* TAG */
+  TravelAgentApp = "travel-agent-app",
+
+  /* Headstart */
+  AtsApp = "ats-app",
+  ScreeningApp = "screening-app",
+
+  /* LL */
+  eCommerceApp = "ecommerce-app",
+
+  /* Personal */
+  Portfolio = "portfolio",
+  TravelAppConcept = "travel-app-concept",
+  CustomReact = "custom-react",
+  DjangoApi = "django-api",
+  OAuthApp = "oauth-app",
+}
+
+enum SkillId {
+  UiUxLiteracy = "ui-ux-literacy",
+  Accessibility = "accessibility",
+  FrontEndArchitecture = "front-end-architecture",
+  FullStackDevelopment = "full-stack-development",
+  DesignSystems = "design-systems",
+  Mentoring = "mentoring",
+  CodeReviews = "code-reviews",
+  Testing = "testing",
+  RestfulApis = "restful-apis",
+  DomainModeling = "domain-modeling",
+  DataModeling = "data-modeling",
+  Pragmatism = "pragmatism",
+  Simplicity = "simplicity",
+  TypeDrivenDevelopment = "type-driven-development",
+}
+
+enum TechId {
+  React = "react",
+  Typescript = "typescript",
+  Node = "node",
+  Express = "express",
+  Nest = "nest",
+  Angular = "angular",
+  TestingLibrary = "testing-library",
+  MongoDB = "mongodb",
+  Postgres = "postgres",
+  GithubActions = "github-actions",
+  Aws = "aws",
+  Azure = "azure",
+  RubyOnRails = "ruby-on-rails",
+}
+
+enum LearnId {
+  OAuthCourse = "oauth-course",
+  AIFundamentalsCourse = "ai-fundamentals-course",
+  DataLiteracyCourse = "data-literacy-course",
+  GraphQLCourse = "graphql-course",
+}
+
+enum WorkId {
+  ISG = "isg",
+  KPMG = "kpmg",
+  Droxic = "droxic",
+  TAG = "tag",
+  Headstart = "headstart",
+  LL = "ll",
+}
