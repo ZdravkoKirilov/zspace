9d4e538cf862f975bb673484954d18d15287a595
zdravkokirilov <zdravkovasilchevkirilov@gmail.com>
2024-02-05 10:21:14 +0200

    wip

--- src/models.ts
+++ src/models.ts
@@ -1,4 +1,4 @@
-enum ProjectId {
+export enum ProjectId {
   /* ISG */
   DynamicFormBuilder = "dynamic-form-builder",
 
@@ -31,7 +31,7 @@ enum ProjectId {
   OAuthApp = "oauth-app",
 }
 
-enum SkillId {
+export enum SkillId {
   UiUxLiteracy = "ui-ux-literacy",
   Accessibility = "accessibility",
   FrontEndArchitecture = "front-end-architecture",
@@ -46,9 +46,11 @@ enum SkillId {
   Pragmatism = "pragmatism",
   Simplicity = "simplicity",
   TypeDrivenDevelopment = "type-driven-development",
+  HybridMobileApps = "hybrid-mobile-apps",
+  ResponsiveDesign = "responsive-design",
 }
 
-enum TechId {
+export enum TechId {
   React = "react",
   Typescript = "typescript",
   Node = "node",
@@ -62,16 +64,24 @@ enum TechId {
   Aws = "aws",
   Azure = "azure",
   RubyOnRails = "ruby-on-rails",
+  Cordova = "cordova",
+  SQLite = "sqlite",
+  Bootstrap = "bootstrap",
+  MaterialUi = "material-ui",
+  CSS = "css",
+  TailwindCss = "tailwind-css",
+  VanillaExtract = "vanilla-extract",
 }
 
-enum LearnId {
+export enum LearnId {
   OAuthCourse = "oauth-course",
   AIFundamentalsCourse = "ai-fundamentals-course",
   DataLiteracyCourse = "data-literacy-course",
   GraphQLCourse = "graphql-course",
+  PostgreSQLCourse = "postgresql-course",
 }
 
-enum WorkId {
+export enum WorkId {
   ISG = "isg",
   KPMG = "kpmg",
   Droxic = "droxic",
@@ -79,3 +89,287 @@ enum WorkId {
   Headstart = "headstart",
   LL = "ll",
 }
+
+export const projects = {
+  [ProjectId.DynamicFormBuilder]: {
+    id: ProjectId.DynamicFormBuilder,
+    type: "work",
+    weight: "highlighted",
+    display_name: "Dynamic Form Builder",
+    skills: [
+      SkillId.HybridMobileApps,
+      SkillId.UiUxLiteracy,
+      SkillId.FrontEndArchitecture,
+      SkillId.DesignSystems,
+      SkillId.Mentoring,
+      SkillId.Testing,
+      SkillId.DomainModeling,
+      SkillId.DataModeling,
+    ] as const,
+    tech: [TechId.Cordova, TechId.SQLite, TechId.Bootstrap],
+    time: {
+      start: new Date("2021-01-01"),
+      end: new Date("2021-01-01") as Date | undefined,
+    },
+  },
+  [ProjectId.D3Analytics]: {
+    id: ProjectId.D3Analytics,
+    type: "work",
+    weight: "normal",
+    display_name: "D3 Analytics",
+    skills: [],
+    tech: [],
+    time: {
+      start: new Date("2021-01-01"),
+      end: new Date("2021-01-01") as Date | undefined,
+    },
+  },
+  [ProjectId.CompanyOrgChart]: {
+    id: ProjectId.CompanyOrgChart,
+    type: "work",
+    weight: "normal",
+    display_name: "Company Org Chart",
+    skills: [],
+    tech: [],
+    time: {
+      start: new Date("2021-01-01"),
+      end: new Date("2021-01-01") as Date | undefined,
+    },
+  },
+  [ProjectId.CompanyPhoneManagement]: {
+    id: ProjectId.CompanyPhoneManagement,
+    type: "work",
+    weight: "normal",
+    display_name: "Company Phone Management",
+    skills: [],
+    tech: [],
+    time: {
+      start: new Date("2021-01-01"),
+      end: new Date("2021-01-01") as Date | undefined,
+    },
+  },
+  [ProjectId.CMSApp]: {
+    id: ProjectId.CMSApp,
+    type: "work",
+    weight: "normal",
+    display_name: "CMS App",
+    skills: [],
+    tech: [],
+    time: {
+      start: new Date("2021-01-01"),
+      end: new Date("2021-01-01") as Date | undefined,
+    },
+  },
+  [ProjectId.FintechApp]: {
+    id: ProjectId.FintechApp,
+    type: "work",
+    weight: "normal",
+    display_name: "Fintech App",
+    skills: [],
+    tech: [],
+    time: {
+      start: new Date("2021-01-01"),
+      end: new Date("2021-01-01") as Date | undefined,
+    },
+  },
+  [ProjectId.IoTApp]: {
+    id: ProjectId.IoTApp,
+    type: "work",
+    weight: "normal",
+    display_name: "IoT App",
+    skills: [],
+    tech: [],
+    time: {
+      start: new Date("2021-01-01"),
+      end: new Date("2021-01-01") as Date | undefined,
+    },
+  },
+  [ProjectId.SportsApp]: {
+    id: ProjectId.SportsApp,
+    type: "work",
+    weight: "normal",
+    display_name: "Sports App",
+    skills: [],
+    tech: [],
+    time: {
+      start: new Date("2021-01-01"),
+      end: new Date("2021-01-01") as Date | undefined,
+    },
+  },
+  [ProjectId.TravelAgentApp]: {
+    id: ProjectId.TravelAgentApp,
+    type: "work",
+    weight: "highlighted",
+    display_name: "Travel Agent App",
+    skills: [],
+    tech: [],
+    time: {
+      start: new Date("2021-01-01"),
+      end: new Date("2021-01-01") as Date | undefined,
+    },
+  },
+  [ProjectId.AtsApp]: {
+    id: ProjectId.AtsApp,
+    type: "work",
+    weight: "highlighted",
+    display_name: "ATS App",
+    skills: [],
+    tech: [],
+    time: {
+      start: new Date("2021-01-01"),
+      end: new Date("2021-01-01") as Date | undefined,
+    },
+  },
+  [ProjectId.ScreeningApp]: {
+    id: ProjectId.ScreeningApp,
+    type: "work",
+    weight: "highlighted",
+    display_name: "Screening App",
+    skills: [],
+    tech: [],
+    time: {
+      start: new Date("2021-01-01"),
+      end: new Date("2021-01-01") as Date | undefined,
+    },
+  },
+  [ProjectId.eCommerceApp]: {
+    id: ProjectId.eCommerceApp,
+    type: "work",
+    weight: "highlighted",
+    display_name: "eCommerce App",
+    skills: [],
+    tech: [],
+    time: {
+      start: new Date("2021-01-01"),
+      end: undefined,
+    },
+  },
+  [ProjectId.Portfolio]: {
+    id: ProjectId.Portfolio,
+    type: "personal",
+    weight: "highlighted",
+    display_name: "Portfolio",
+    skills: [],
+    tech: [],
+    time: {
+      start: new Date("2021-01-01"),
+      end: new Date("2021-01-01"),
+    },
+  },
+  [ProjectId.TravelAppConcept]: {
+    id: ProjectId.TravelAppConcept,
+    type: "personal",
+    weight: "highlighted",
+    display_name: "Travel App Concept",
+    skills: [],
+    tech: [],
+    time: {
+      start: new Date("2021-01-01"),
+      end: new Date("2021-01-01"),
+    },
+  },
+  [ProjectId.CustomReact]: {
+    id: ProjectId.CustomReact,
+    type: "personal",
+    weight: "normal",
+    display_name: "Custom React",
+    skills: [],
+    tech: [],
+    time: {
+      start: new Date("2021-01-01"),
+      end: new Date("2021-01-01"),
+    },
+  },
+  [ProjectId.DjangoApi]: {
+    id: ProjectId.DjangoApi,
+    type: "personal",
+    weight: "normal",
+    display_name: "Django API",
+    skills: [],
+    tech: [],
+    time: {
+      start: new Date("2021-01-01"),
+      end: new Date("2021-01-01"),
+    },
+  },
+  [ProjectId.OAuthApp]: {
+    id: ProjectId.OAuthApp,
+    type: "personal",
+    weight: "highlighted",
+    display_name: "OAuth App",
+    skills: [],
+    tech: [],
+    time: {
+      start: new Date("2021-01-01"),
+      end: new Date("2021-01-01"),
+    },
+  },
+} as const;
+
+export const technologies = {
+  [TechId.React]: {
+    id: TechId.React,
+    display_name: "React",
+    weight: "highlighted",
+    projects: [
+      ProjectId.CompanyOrgChart,
+      ProjectId.IoTApp,
+      ProjectId.SportsApp,
+      ProjectId.FintechApp,
+      ProjectId.CustomReact,
+      ProjectId.AtsApp,
+      ProjectId.ScreeningApp,
+      ProjectId.eCommerceApp,
+    ] as const,
+  },
+  courses: [],
+} as const;
+
+export const skills = {
+  [SkillId.UiUxLiteracy]: {
+    id: SkillId.UiUxLiteracy,
+    display_name: "UI/UX Literacy",
+    weight: "highlighted",
+    projects: [
+      ProjectId.CMSApp,
+      ProjectId.TravelAgentApp,
+      ProjectId.Portfolio,
+      ProjectId.TravelAppConcept,
+      ProjectId.eCommerceApp,
+      ProjectId.ScreeningApp,
+    ],
+    technologies: [
+      TechId.React,
+      TechId.Angular,
+      TechId.Bootstrap,
+      TechId.MaterialUi,
+      TechId.CSS,
+      TechId.TailwindCss,
+      TechId.VanillaExtract,
+    ],
+  },
+} as const;
+
+export const learnings = {
+  [LearnId.OAuthCourse]: {
+    id: LearnId.OAuthCourse,
+    display_name: "OAuth Course",
+    weight: "highlighted",
+    provider: {
+      name: "Datacamp",
+      url: "https://www.datacamp.com/",
+    },
+    time: {
+      start: new Date("2021-01-01"),
+      end: new Date("2021-01-01") as Date | undefined,
+    },
+    skills: [
+      SkillId.RestfulApis,
+      SkillId.FullStackDevelopment,
+      SkillId.DomainModeling,
+      SkillId.DataModeling,
+      SkillId.TypeDrivenDevelopment,
+    ],
+    projects: [ProjectId.OAuthApp],
+  },
+} as const;
---
+++ src/utils/dates.ts
@@ -0,0 +1,92 @@
+type Interval = {
+  start: Date;
+  end: Date | undefined;
+};
+
+const isNonEmptyArray = <T>(arr: T[]): arr is [T, ...T[]] => arr.length > 0;
+
+// Function to merge overlapping intervals and handle null end times
+function mergeIntervals<T extends Interval>(data: T[]): T[] {
+  if (!isNonEmptyArray(data)) return [];
+
+  // Substitute null end times with the current date
+  const intervals = data.map(
+    (interval) =>
+      ({
+        start: interval.start,
+        end: interval.end ?? new Date(), // Use current date if end is null
+      } as const)
+  ) as T[];
+
+  // Sort intervals by their start date
+  intervals.sort((a, b) => a.start.getTime() - b.start.getTime());
+
+  const firstInterval = intervals[0];
+
+  if (!firstInterval) {
+    return [];
+  }
+
+  const merged: Interval[] = [firstInterval];
+
+  for (let i = 1; i < intervals.length; i++) {
+    const last = merged[merged.length - 1];
+    const current = intervals[i];
+
+    if (!last || !current) continue;
+
+    if (current.start <= (last.end ?? new Date())) {
+      last.end = new Date(
+        Math.max(
+          (last.end ?? new Date()).getTime(),
+          (current.end ?? new Date()).getTime()
+        )
+      );
+    } else {
+      merged.push(current);
+    }
+  }
+
+  return merged as T[];
+}
+
+function calculateTotalElapsedTime(intervals: Interval[]): number {
+  const mergedIntervals = mergeIntervals(intervals);
+
+  return mergedIntervals.reduce((total, interval) => {
+    return (
+      total +
+      ((interval.end ?? new Date()).getTime() - interval.start.getTime())
+    );
+  }, 0);
+}
+
+type RelativeResult = { years: number; rounding: "below" | "above" | "exact" };
+
+export const getElapsedTimeInYears = <T extends Interval>(
+  intervals: T[]
+): RelativeResult => {
+  const years =
+    calculateTotalElapsedTime(intervals) / (1000 * 60 * 60 * 24 * 365);
+  const rounded = Math.round(years);
+  const modulus = years % rounded;
+
+  if (modulus === 0) {
+    return {
+      years: rounded,
+      rounding: "exact",
+    } as const;
+  }
+
+  if (modulus > 0.5) {
+    return {
+      years: rounded,
+      rounding: "above",
+    } as const;
+  }
+
+  return {
+    years: rounded,
+    rounding: "below",
+  } as const;
+};
